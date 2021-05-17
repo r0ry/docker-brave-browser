d1291799742d6ccfef1d3da454d75b89a44b5ba9
Georgi Stoev <r0ryy@yahoo.com>
2021-05-17 22:17:28 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 sudo docker buid . -t brave:v1
 for ((i=1; i<=$1; i=i+1))
 do 
-    echo "Instance number building: " + $i
+    echo "Instance number : " $i
     sudo docker run -d --rm -it --privileged --env DISPLAY=unix$DISPLAY -v $XAUTH:/root/.Xauthority -v /tmp/.X11-unix:/tmp/.X11-unix -p 500$i:500$i brave:v1
 done
